de5d67e8ed887e5f5810f6f79019b26da636ab4a
Sean Kelley <s.kelley27@gmail.com>
2016-01-07 14:41:07 -0600

    create listener for touch start and fire the new 'touchDeviceDetected' event

--- touchDeviceChecker.js
+++ touchDeviceChecker.js
@@ -1,8 +1,12 @@
 (function($){
 	'use strict';
+	var hasTouch = false;
 	$.fn.checkForTouch = function(options){
 		var $element = $(this);
-		
+		window.addEventListener('touchstart', function setHasTouch () {
+			hasTouch = true;
+			$element.trigger('touchDeviceDetected', []);
+			window.removeEventListener('touchstart', setHasTouch);
+		}, false);
 	};
-
-});
+})(jQuery);
